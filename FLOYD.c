29b0e4fea5faaf2127c0bd68f94a40b88c951b1b
JOEL PRAKASH <79404155+joelprakash876@users.noreply.github.com>
2022-09-05 12:53:28 +0530

    Create FLOYD.c

---
+++ FLOYD.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int min(int a,int b){
+return (a<b) ? a : b;
+}
+int main(){
+int i,j,k,n;
+int a[10][10];
+printf("Enter the no of vertices\n");
+scanf("%d",&n);
+printf("Enter the adjacency matrix\n");
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+scanf("%d",&a[i][j]);
+}
+}
+for(k=0;k<n;k++){
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+a[i][j] = min(a[i][j],a[i][k]+a[k][j]);
+}
+}
+}
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+printf("%d ",a[i][j]);
+}
+printf("\n");
+}
+
+}
