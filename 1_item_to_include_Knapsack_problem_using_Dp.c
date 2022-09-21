5c0b4970734869debbeca621c4be98ec8770b847
JOEL PRAKASH <79404155+joelprakash876@users.noreply.github.com>
2022-09-21 23:16:48 +0530

    Create 1_item_to_include_Knapsack_problem_using_Dp.c

---
+++ 1_item_to_include_Knapsack_problem_using_Dp.c
@@ -0,0 +1,33 @@
+/*1.	Given a set of items, each with a weight and a value, determine the number of each item to include
+in a collection so that the total weight is less than or equal to a given limit and the total value is as large as
+possible. (0/1 Knapsack problem using DP) */
+
+#include <stdio.h>
+int max(int a, int b) 
+{ 
+return (a > b) ? a : b;
+}
+int knapSack(int W, int wt[], int val[], int n)
+{
+    if (n == 0 || W == 0)
+        return 0;
+    if (wt[n - 1] > W)
+        return knapSack(W, wt, val, n - 1);
+    else
+        return max(
+            val[n - 1]
+                + knapSack(W - wt[n - 1],
+                           wt, val, n - 1),
+            knapSack(W, wt, val, n - 1));
+}
+ 
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("%d", knapSack(W, wt, val, n));
+    return 0;
+    }
+}
