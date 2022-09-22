c80465933461414e5be0862e2266bd6f4a8a2a95
JOEL PRAKASH <79404155+joelprakash876@users.noreply.github.com>
2022-09-22 07:23:43 +0530

    Update 2_PRIMS_subset_of_the_edges.c

--- 2_PRIMS_subset_of_the_edges.c
+++ 2_PRIMS_subset_of_the_edges.c
@@ -1,48 +1,132 @@
 /* 2.	Given a weighted undirected graph. Finds a subset of the edges that forms a tree that includes every
 vertex, where the total weight of all the edges in the tree is minimized by using prims approach. */
 
-#include<stdio.h>
+prims
+
+#include <stdio.h>
+
+int prims(int cost[10][10],int n,int t[10][10])
+
+{
+
+int min=9999,k,l,near[10],j,mincost=0,i,index;
+
+for(i=1;i<=n;i++)
+
+{
+
+for(j=i+1;j<=n;j++)
+
+{
+
+if(cost[i][j]<min&&cost[i][j]!=0)
+
+{
+
+min=cost[i][j];
+
+k=i;
+
+l=j;
+
+}
+
+}
+
+}
+
+t[1][1]=k;
+
+t[1][2]=l;
+
+mincost=mincost+cost[k][l];
+
+for(i=1;i<=n;i++)
+
+{
+
+if(cost[i][l]<cost[i][k])
+
+near[i]=l;
+
+else
+
+near[i]=k;
+
+}
+
+near[k]=0;
+
+near[l]=0;
+
+for(i=2;i<=n-1;i++)
+
+{
+
+min=9999;
+
+for(j=1;j<=n;j++)
+
+{
+
+if(near[j]!=0&&cost[j][near[j]]<min){
+
+   min=cost[j][near[j]];
+
+   index=j;
+
+}
+
+}
+
+t[i][1]=index;
+
+t[i][2]=near[index];
+
+mincost=mincost+cost[index][near[index]];
+
+   near[index]=0;
+
+for(k=1;k<=n;k++)
+
+{
+
+if(near[k]!=0&&cost[k][near[k]]>cost[k][index])
+
+near[k]=index;
+
+}
+
+}
+
+return mincost;
+
+}
+
 int main()
+
 {
-int p[20],bt[20], su[20], wt[20],tat[20],i, k, n, temp;
-float wtavg, tatavg;
-printf("Enter the number of processes:");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-p[i] = i;
-printf("Enter the Burst Time of Process%d:", i);
-scanf("%d",&bt[i]);
-printf("System/User Process (0/1) ? ");
-scanf("%d", &su[i]);
-}
-for(i=0;i<n;i++)
-for(k=i+1;k<n;k++)
-if(su[i] > su[k])
-{
-temp=p[i];
-p[i]=p[k];
-p[k]=temp;
-temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-temp=su[i];
-su[i]=su[k];
-su[k]=temp;
-}
-wtavg = wt[0] = 0;
-tatavg = tat[0] = bt[0];
-for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] + bt[i-1];
-tat[i] = tat[i-1] + bt[i];
-wtavg = wtavg + wt[i];
-tatavg = tatavg + tat[i];
-}
-printf("\nPROCESS\tS/U\tBURST\tWAITING\tTURNAROUND");
-for(i=0;i<n;i++)
-printf("\n%d\t%d\t%d\t%d\t%d",p[i],su[i],bt[i],wt[i],tat[i]);
-printf("\nAverage Waiting Time is --- %.2f\n",wtavg/n);
-printf("Average Turnaround Time is --- %.2f\n",tatavg/n);
+
+    int n,t[10][10],mincost=0;
+
+    printf("Enter n value:");
+
+    scanf("%d",&n);
+
+    printf("Enter cost matrix\n");
+
+int i,j,c[10][10];
+
+for(i=1;i<=n;i++)
+
+for(j=1;j<=n;j++)
+
+scanf("%d",&c[i][j]);
+
+mincost=prims(c,n,t);
+
+printf("Minimum cost=%d\n",mincost);
+
 return 0;
+
 }
