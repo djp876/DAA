3fdd9917760077bd4c64f63ff87feb8b689a25cf
JOEL PRAKASH <79404155+joelprakash876@users.noreply.github.com>
2022-09-22 11:31:16 +0530

    Update 7_sd_betwween_Fpair_of_vertices_FLOYD.c

--- 7_sd_betwween_Fpair_of_vertices_FLOYD.c
+++ 7_sd_betwween_Fpair_of_vertices_FLOYD.c
@@ -1,32 +1,91 @@
 /* 7.	For a weighted directed Graph, find shortest distances between every pair of vertices. (Floyds) */
 
+Floyd's algorithm
+
 #include<stdio.h>
-int min(int a,int b){
-return (a<b) ? a : b;
+
+int n,cost[10][10],A[10][10];
+
+void floyds(int cost[10][10],int A[10][10],int n)
+
+{
+
+    int i,j,k,min;
+
+    for(i=1;i<=n;i++)
+
+    {
+
+        for(j=1;j<=n;j++)
+
+        A[i][j]=cost[i][j];
+
+    }
+
+    for(k=1;k<=n;k++)
+
+    {
+
+        for(i=1;i<=n;i++)
+
+        {
+
+            for(j=1;j<=n;j++)
+
+            {
+
+                if(A[i][k]+A[k][j]<A[i][j])
+
+                A[i][j]=A[i][k]+A[k][j];
+
+            }
+
+        }
+
+    }
+
+    printf("The shortest paths are:\n");
+
+   for(i=1;i<=n;i++)
+
+   {
+
+       for(j=1;j<=n;j++){
+
+          printf("%d\t",A[i][j]);
+
+       }
+
+       printf("\n");
+
+   }
+
 }
-int main(){
-int i,j,k,n;
-int a[10][10];
-printf("Enter the no of vertices\n");
-scanf("%d",&n);
-printf("Enter the adjacency matrix\n");
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-scanf("%d",&a[i][j]);
-}
-}
-for(k=0;k<n;k++){
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-a[i][j] = min(a[i][j],a[i][k]+a[k][j]);
-}
-}
-}
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-printf("%d ",a[i][j]);
-}
-printf("\n");
-}
+
+int main()
+
+{
+
+    int i,j;
+
+    printf("Enter no.of vertices:");
+
+    scanf("%d",&n);
+
+    printf("Enter cost matrix\n");
+
+    for(i=1;i<=n;i++)
+
+    {
+
+        for(j=1;j<=n;j++)
+
+        scanf("%d",&cost[i][j]);
+
+    }
+
+    floyds(cost,A,n);
+
+    return 0;
 
 }
