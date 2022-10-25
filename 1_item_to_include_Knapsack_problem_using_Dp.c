7cd16d61e2b264a05f4fa9a54cf909bebdf98569
DJP <79404155+djp876@users.noreply.github.com>
2022-10-26 01:24:23 +0530

    Update 1_item_to_include_Knapsack_problem_using_Dp.c

--- 1_item_to_include_Knapsack_problem_using_Dp.c
+++ 1_item_to_include_Knapsack_problem_using_Dp.c
@@ -1,4 +1,4 @@
-/*1.	Given a set of items, each with a weight and a value, determine the number of each item to include
+/*1.Given a set of items, each with a weight and a value, determine the number of each item to include
 in a collection so that the total weight is less than or equal to a given limit and the total value is as large as
 possible. (0/1 Knapsack problem using DP) */
 
@@ -30,4 +30,3 @@ int main()
     printf("%d", knapSack(W, wt, val, n));
     return 0;
     }
-}
